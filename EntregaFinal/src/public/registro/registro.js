5d538ef56b5765a069bc31f39bcf95d7b28fc072
César Carabetta <carabetta87@gmail.com>
2022-05-29 19:27:24 -0300

    Mejorado registro

--- EntregaFinal/src/public/registro/registro.js
+++ EntregaFinal/src/public/registro/registro.js
@@ -6,39 +6,48 @@ function validar(e) {
   
   let email = document.querySelector("#email").value;
   let pass = document.querySelector("#password").value;
+  let nombre = document.querySelector("#nombre").value;
+  let passCheck = document.querySelector("#passwordCheck").value;
   let error = document.querySelector('#error');
   
   let userInfo = {
+      "nombre": nombre,
       "email": email,
       "password": pass
   }
     
     if (email != "" && pass != "") {
-      try {
-        fetch(`http://localhost:3019/api/users`, {
-          method: "POST",
-          redirect: 'follow',
-          mode: "cors", // no-cors, *cors, same-origin
-          cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-          credentials: "same-origin", // include, *same-origin, omit
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(userInfo),      
-        })                   
-        //   .then((response) => response.json())
-          .then((res) => {
-            if(res.status == 400){
-              window.location.replace('/api/registroFail')
-            }else{
-              window.location.replace('/api/login')
-            }
-            console.log(res.status)
-          })        
-          .catch((error) => console.log(error));
-      } catch (error) {
-        console.log(error);
+      if(pass === passCheck){
+        try {
+          fetch(`http://localhost:3019/api/users`, {
+            method: "POST",
+            redirect: 'follow',
+            mode: "cors", // no-cors, *cors, same-origin
+            cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+            credentials: "same-origin", // include, *same-origin, omit
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(userInfo),      
+          })                   
+          //   .then((response) => response.json())
+            .then((res) => {
+              if(res.status == 400){
+                window.location.replace('/api/registroFail')
+              }else{
+                window.location.replace('/api/login')
+              }
+              console.log(res.status)
+            })        
+            .catch((error) => console.log(error));
+        } catch (error) {
+          console.log(error);
+        }
+      } else {
+        error.innerHTML = '<div class="error">Las contraseñas no coinciden</div>'
+        return;
       }
+      
     }else{
       error.innerHTML = '<div class="error">Debe completar todos los campos</div>'
     }
